0397318e52e5e0564b782c0e4672bde345674814
junyonglee0223 <ljyong3339@gmail.com>
2025-03-21 14:29:06 +0900

    feat: app V2: toss traceId

--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -3,8 +3,10 @@ package hello.advanced.trace.hellotrace;
 import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class HelloTraceV2 {
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
