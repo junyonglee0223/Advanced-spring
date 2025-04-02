d9118ec4bbf0df37f9600396d46b1390d35d114b
junyonglee0223 <ljyong3339@gmail.com>
2025-04-02 10:53:42 +0900

    test: AbstractTemplate

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+    @Test
+    void templateMethodV0(){
+        logic1();
+        logic2();
+    }
+
+    private void logic1(){
+        long startTime = System.currentTimeMillis();
+
+        log.info("business logic1 processing!!!");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+    private void logic2(){
+        long startTime = System.currentTimeMillis();
+
+        log.info("business logic2 processing!!!");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1 = new SubClassLogic1();
+        AbstractTemplate template2 = new SubClassLogic2();
+
+        template1.execute();
+        template2.execute();
+    }
+}
